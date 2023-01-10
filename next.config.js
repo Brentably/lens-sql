90f0981fa1ad5f7762bafab144268e1a362881f0
Brentably <Brent@pabnet.com>
2023-01-10 12:06:31 -0500

    setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+ 
+dotenvLoad('.env.local');
+
+ 
+const withNextEnv = nextEnv();
+ 
+module.exports = withNextEnv(nextConfig);
