ff6e5e5fb0f824ef3119e941d812f623e8e159ed
Brentably <Brent@pabnet.com>
2023-01-10 14:41:25 -0500

    decent setup where it will query openAI via the api now

---
+++ pages/api/genSQL.ts
@@ -0,0 +1,47 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { OpenAIApi, Configuration } from 'openai'
+
+//maybe innefficient? I'm not sure if this base code runs on every server call
+
+
+
+// type Data = {
+//   name: string
+// }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const configuration = new Configuration({
+    apiKey: process.env.NEXT_SERVER_OPENAI,
+  });
+  const openai = new OpenAIApi(configuration);
+
+
+  const prompt = JSON.parse(req.body).prompt;
+
+
+
+try{
+  console.log('try')
+  const response = await openai.createCompletion({
+    model: "code-davinci-002",
+    prompt: `### MySQL SQL tables, with their properties:\n#\n# Employee(id, name, department_id)\n# Department(id, name, address)\n# Salary_Payments(id, employee_id, amount, date)\n# lens_follow_view(profileId, address, handle, rootProfileId, rootAddress, rootHandle, timestamp)\n# lens_profile_view(profileId, metadata, handle, address, imageURI, timestamp)\n# lens_publication_comment_view(profileId, address, handle, pubId, contentURI, rootProfileId, rootAddress, rootHandle, type, timestamp)\n# lens_publication_mirror_view(profileId, address, handle, pubId, contentURI, rootProfileId, rootAddress, rootHandle, type, timestamp)\n# lens_publication_post_view(profileId, address, handle, pubId, contentURI, rootProfileId, rootAddress, rootHandle, type, timestamp)\n# lens_publication_view(profileId, address, handle, pubId, contentURI, rootPRofileId, rootAddress, rootHandle, type)\n#\n### ${prompt}\nSELECT`,
+    temperature: 0,
+    max_tokens: 150,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: ["#", ";"],
+  });
+
+  res.status(200).json({data: response.data})
+} catch (error) {
+  console.log(error)
+  res.status(500).json(error)
+}
+
+
+}
