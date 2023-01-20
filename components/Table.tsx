364510303daa75883cc8e16ccdf67be5254f1d5e
Brentably <Brent@pabnet.com>
2023-01-20 11:00:30 -0500

    got nice table ui working

---
+++ components/Table.tsx
@@ -0,0 +1,29 @@
+
+const Table = ({data}:{data: any[]}) => {
+const keys = Object.keys(data[0])
+
+
+  return (
+    <div className="text-center">
+      {/* {JSON.stringify(data)} */}
+      <table className="mx-auto p-2 rounded-xl">
+        <thead>
+          {keys.map((key, i) => 
+            <th key={i} className="border border-slate-400 p-3">{key}</th>
+          )}
+        </thead>
+        <tbody>
+          {data.map((row, i) => 
+          <tr key={i}>
+            {keys.map((key, i) => 
+              <td key={i} className="border border-slate-400 p-3">{row[key]}</td>
+            )}
+          </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
