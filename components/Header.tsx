473a049e4d05ad79d4340ac84f006178764d10b9
Brentably <Brent@pabnet.com>
2023-02-15 18:46:44 -0500

    added header buttons and routes

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const Header = () => {
+const router = useRouter()
+console.log(router)
+
+  return (
+  <div className="flex row justify-between items-center m-3">
+    <div className="flex row items-center gap-5">
+      <div className='bg-gray-500 w-20 h-20 rounded-full flex items-center justify-center'>Logo</div>
+      <div className="text-red-600 text-3xl font-semibold">EASY QUERY</div>
+      
+      <div className="flex row self-stretch">
+
+        <Link href='/' 
+          className={router.pathname == '/' ? 'bg-red-600 text-white flex items-center my-3 p-5' 
+          : 'bg-red-600 text-gray-600 flex items-center my-3 p-5'}>
+            Home
+        </Link>
+        <Link href='/query' 
+          className={router.pathname == '/query' ? 'bg-red-600 text-white flex items-center my-3 p-5' 
+          : 'bg-red-600 text-gray-600 flex items-center my-3 p-5'}>
+            Query
+        </Link>
+      </div>
+    </div>
+    <div className="bg-red-600 text-white p-2 rounded-lg">Connect Wallet</div>
+  </div>
+  )
+}
+
+export default Header
