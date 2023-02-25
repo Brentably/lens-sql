9f5360c8c8c1f04d13e92b202442c9b2e0bb97dc
李杰 <827188236@qq.com>
2023-02-25 21:10:03 +0800

    add package

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 // module.exports = nextConfig
