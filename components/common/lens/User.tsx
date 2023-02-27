244fc45810e303ce7a4a70eacdb6d610de5b457a
Brentably <Brent@pabnet.com>
2023-02-27 14:24:02 -0700

    started to render a llittle bit of content

--- components/common/lens/User.tsx
+++ components/common/lens/User.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import LensImg from '../../../statics/img/lest-head.png'
 
-export default function User() {
+export default function User(props) {
+  const {user_name} = props
   return (
     <div className='shadow flex w-[fit-content] px-3 py-2 pr-10 rounded-[26px] cursor-pointer mb-3'>
               <div className='h-[40px] w-[40px] rounded-[50%] mr-3'>
@@ -12,8 +13,10 @@ export default function User() {
                 />
               </div>
               <div>
-                <p className='text-[14px]'>KNN3 Network</p>
+                <p className='text-[14px]'>{user_name}</p>
                 <p className='text-[12px]'>@knn3_network</p>
+                {/* <p className='text-[14px]'>KNN3 Network</p>
+                <p className='text-[12px]'>@knn3_network</p> */}
               </div>
       </div>
   )
