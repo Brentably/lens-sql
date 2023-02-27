f1be26a4bd98404dddd0b9bf2b367d7d0fb00c8e
Brentably <Brent@pabnet.com>
2023-02-27 11:09:57 -0700

    Add logic to prevent running query when wallet is not connected

---
+++ components/common/ShowFileTip.tsx
@@ -0,0 +1,21 @@
+ export {}
+ /* { NOT using this code anymore cause queries don't need names
+        showFileTip &&
+        <div className='w-[460px] border-[2px] border-[#000] database-alert bg-[#fff] rounded-[10px] p-8'>
+          <div className='flex'>
+            <div className='w-[80%]'>Save file as</div>
+            <Image
+              className="cursor-pointer h-[40px] w-[40px] ml-[auto]"
+              onClick={() => setShowFileTip(false)}
+              src={Close}
+              alt=""
+            />
+          </div>
+          <div className='border-[2px] border-[#000] px-2 py-2 rounded-[10px] mt-10'>
+            <input placeholder='New File' value={fileName} onChange={(e) => setFileName(e.target.value)}></input>
+          </div>
+          <div className='flex justify-end mt-10'>
+          <button className="h-[40px] w-[120px] bg-[#000] hover:bg-[#181EFF] text-[#fff] flex justify-center items-center rounded-[10px] mr-[20px] cursor-pointer" onClick={handleSave}>Save</button>
+          </div>
+        </div>
+      } */
