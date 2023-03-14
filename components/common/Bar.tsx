41c028dd2eebc766b7e2ba334eea9025f92e6b82
李杰 <827188236@qq.com>
2023-03-14 13:35:39 +0800

    add insights

--- components/common/Bar.tsx
+++ components/common/Bar.tsx
@@ -3,33 +3,50 @@ import * as echarts from 'echarts';
 
 const Bar = (props: any) => {
 
-    const { id = 'default-bar-id', width = '100%', height = '100%',data } = props;
+    const { id = 'default-line-id', width = '100%', height = '100%', data } = props;
+    let series: any = [], xData: any = [], xKey = ''
+    const keys = Object.keys(props.data[0])
+    keys.map((t) => {
+        let yData: any = {
+            name: t,
+            data: [],
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+                color: 'rgba(180, 180, 180, 0.2)'
+            }
+        }
+        props.data.forEach((tem: any) => {
+            if (t.includes('count') || t.includes('num') || t.includes('average')) {
+                yData.data.push(tem[t])
+            } else {
+                xData.push(tem[t])
+            }
+
+        })
+        series.push(yData)
+    })
 
     useEffect(() => {
         const option = {
             xAxis: {
                 type: 'category',
-                data: data.xData
+                data: xData
             },
             yAxis: {
                 type: 'value'
             },
-            grid:{
-                top:30,
-                bottom:30,
-                left:'5%',
-                right:'5%',
+            grid: {
+                top: 30,
+                bottom: 30,
+                left: 50,
+                right: 0,
             },
-            series: [
-                {
-                    data: data.yData,
-                    type: 'bar',
-                    showBackground: true,
-                    backgroundStyle: {
-                        color: 'rgba(180, 180, 180, 0.2)'
-                    }
-                }
-            ]
+            tooltip: {
+                show: true,
+                trigger: 'axis',
+            },
+            series: series
         };
 
         const HTMLElement = document.getElementById(id) as HTMLElement;
--- components/common/Line.tsx
+++ components/common/Line.tsx
@@ -1,31 +1,62 @@
 import { useEffect } from 'react';
 import * as echarts from 'echarts';
 
-const Bar = (props: any) => {
+const Line = (props) => {
 
     const { id = 'default-line-id', width = '100%', height = '100%', data } = props;
+    let xParams = ['day', 'date', 'week', 'month','timestamp'];
+    let yParams = ['mirrors','posts','comments','likes']
+    let series:any = [], xData:any = [], xKey = ''
+    const keys = Object.keys(props.data[0])
+    keys.map((t) => {
+        if (xParams.includes(t)) {
+            xKey = t
+        }else{
+            if(t.includes('count') || t.includes('num') || t.includes('average') || yParams.includes(t)){
+                let yData:any = {
+                    name:t,
+                    data:[],
+                    type: 'line'
+                }
+                props.data.forEach((tem: any) => {
+                    yData.data.push(tem[t])
+                })
+                series.push(yData)
+            }
+        }
+    })
+    if(!xKey) return (<></>)
+    if(xKey === 'timestamp'){
+        let data = props.data.sort((a, b) => a.timestamp.localeCompare(b.timestamp));
+        data.forEach((t: any) => {
+            xData.push(t[xKey].slice(0,10))
+        })
+    }else{
+        props.data.forEach((t: any) => {
+            xData.push(t[xKey])
+        })
+    }
 
     useEffect(() => {
         const option = {
             xAxis: {
                 type: 'category',
-                data: data.xData
+                data: xData
             },
-            grid:{
-                top:30,
-                bottom:30,
-                left:'5%',
-                right:'5%',
+            grid: {
+                top: 30,
+                bottom: 30,
+                left: 50,
+                right: 0,
+            },
+            tooltip: {
+                show: true,
+                trigger: 'axis',
             },
             yAxis: {
                 type: 'value'
             },
-            series: [
-                {
-                    data: data.yData,
-                    type: 'line'
-                }
-            ]
+            series: series
         };
 
         const HTMLElement = document.getElementById(id) as HTMLElement;
@@ -50,4 +81,4 @@ const Bar = (props: any) => {
 
 };
 
-export default Bar;
+export default Line;
