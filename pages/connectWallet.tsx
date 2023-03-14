0408ed7bb53a7a3dcd010bc37afe70f7dd4befa0
李杰 <827188236@qq.com>
2023-03-14 22:56:10 +0800

    modify

--- pages/connectWallet.tsx
+++ pages/connectWallet.tsx
@@ -1,10 +1,26 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import Image from 'next/image'
 import ImgBrowser from '../statics/img/browser.svg'
 import ImgWallet from '../statics/img/wallet.svg'
 import ImgLogo from '../statics/img/logo.png'
+import { useAppState, useConnectWallet } from '@web3-onboard/react'
+import { useRouter } from "next/router";
 
 export default function ConnectWallet() {
-    
+
+    const router = useRouter()
+
+    const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+
+    const [address, setAddress] = useState<string>('')
+
+    useEffect(() => {
+        //   setAddress(wallet?.accounts[0].address || "")
+        if (wallet?.accounts[0].address) {
+            router.push('/querySql')
+        }
+    }, [wallet])
+
     return (
         <>
             <div className="container mx-auto flex items-center gap-16 h-screen w-screen bg-[#fff] font-[Arial]">
@@ -16,9 +32,9 @@ export default function ConnectWallet() {
                 </div>
                 <div className="rounded-[45px] w-1/2 h-5/6 bg-[#fff] answer-shadow py-[60px] px-[54px] flex items-center justify-center">
                     <div>
-                        
-                            <Image alt={''} src={ImgLogo}  className="h-[100px] w-[100px] bg-gray2 rounded-[24px] flex items-center justify-center answer-shadow mx-[auto]"/>
-                  
+
+                        <Image alt={''} src={ImgLogo} className="h-[100px] w-[100px] bg-gray2 rounded-[24px] flex items-center justify-center answer-shadow mx-[auto]" />
+
                         <p className="text-center text-[28px] font-[700] my-5">
                             Connect Your Wallet
                         </p>
@@ -27,20 +43,12 @@ export default function ConnectWallet() {
                             one.
                         </p>
                         <div
-                            className="flex w-[80%] items-center mx-[auto] h-[60px] bg-gray2 rounded-[25px] answer-shadow px-5 mt-5 hover:opacity-70 cursor-pointer"
+                            className="flex w-[80%] justify-center items-center mx-[auto] h-[60px] bg-gray2 rounded-[25px] answer-shadow px-5 mt-5 hover:opacity-70 cursor-pointer"
                         >
-                            <div className="mr-[auto]">Browser Wallet</div>
-                            <div>
+                            <div onClick={() => connect()}>Connect Wallet</div>
+                            {/* <div>
                                 <Image alt={''} src={ImgBrowser} />
-                            </div>
-                        </div>
-                        <div
-                            className="flex w-[80%] items-center mx-[auto] h-[60px] bg-gray2 rounded-[25px] answer-shadow px-5 mt-5 hover:opacity-70 cursor-pointer"
-                        >
-                            <div className="mr-[auto]">WalletConnect</div>
-                            <div>
-                                <Image alt={''} src={ImgWallet} />
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
